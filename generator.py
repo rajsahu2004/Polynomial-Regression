041526ae124a2ab31949961309618fc54fa75c43
Raj Sahu <sahuraj457@gmail.com>
2022-12-31 20:53:19 +0530

    completed

--- generator.py
+++ generator.py
@@ -6,15 +6,10 @@ def x2(x):
 def x3(x):
     return 1+x**2+x**3
 
+
 # Opening file system
 filex2 = open('data/datax2', "w+")
 filex2.write('Number,Value\n')
 for i in range(1000):
     filex2.write(f'{(i)},{i**2}\n')
 filex2.close()
-
-filex3 = open('data/datax3', "w+")
-filex3.write('Number,Value\n')
-for i in range(1000):
-    filex3.write(f'{(i)},{i**3}\n')
-filex3.close()
